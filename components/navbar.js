70b6820790501a2f2b7292bce2e61dc83ae7bf33
Lochan Jangid <87118270+lochan-code@users.noreply.github.com>
2023-05-02 02:35:04 -0700

    🚀🚀🚀🚀🚀🚀🚀🚀

--- components/navbar.js
+++ components/navbar.js
@@ -4,18 +4,13 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <>
-       <div className='z-50 transition-all glass p-1 space-x-2 flex justify-center items-center fixed right-0 top-10'>
-     <a className=' hover:scale-75transition-all hover:rotate-12' href="tel:9983760244">
-      <Image alt='photo' src={"/icons/call.png"}  width={40} height={40} />
-     </a>
-     <a className=' hover:scale-75 transition-all hover:rotate-12' href="mailto:mtsschool1998@gmail.com">
-      <Image alt='photo' src={"/icons/mail.png"}  width={40} height={40} />
-     </a>
-    </div>
-    <div className='z-50 transition-all glass md:p-5 p-2 h-15 space-x-2 flex justify-center items-center fixed bottom-4 md:h-20 left-1/2 -translate-x-1/2'>
+    <div className='fixed transition-all glass md:p-5 p-2 h-15 w-full space-x-2 flex items-center bottom-0 md:h-20'>
+      <div>
+      <Image alt='MTS' src='/logo/black-transparent.png' width={100} height={40} />
+      </div>
+      <div className="flex absolute right-2 space-x-2">
       <Link href={'/'}>
       <div className='transition-all icon-parent'>
-        <p className='glass-blur text-center text-black -mb-6 text-xl p-3'>Home</p>
       <div className=" glass-blur md:p-3 p-1 flex justify-center items-center cursor-pointer">
         <Image alt='photo'  src="/icons/home.png" width={30} height={30} />
       </div>
@@ -23,15 +18,22 @@ const Navbar = () => {
       </Link>
       <Link href={'/about'}>
         <div className='transition-all icon-parent'>
-        <p className=' glass-blur text-center text-black -mb-6 text-xl p-3'>About</p>
-      <div className=" glass-blur md:p-3 p-1 flex justify-center items-center cursor-pointer">
+          <ul className='glass text-center flex flex-col text-black fixed right-40 text-xl p-3'>
+        <Link href={'/vnm'}><li className='glass p-1 m-1'>VISION AND MISSION</li></Link>
+        <Link href={'/story'}><li className='glass p-1 m-1'>OUR STORY</li></Link>
+        {/* <Link href={'/'}><li className='glass p-1 m-1'>ACHIEVEMENTS</li></Link> */}
+      </ul>
+      <div className=" glass-blur md:p-3 p-1 flex justify-center items-center cursor-pointer ">
         <Image alt='photo'  src="/icons/about.png" width={30} height={30} />
       </div>
       </div>
       </Link>
       <Link href={'/addmission'}>
         <div className='transition-all icon-parent'>
-        <p className=' glass-blur text-center text-black -mb-6 text-xl p-3'>Addmission</p>
+        <ul className='glass text-center flex flex-col text-black fixed right-14 text-xl p-3'>
+        <Link href={'/process'}><li className='glass p-1 m-1'>ADMISSION PROCESS AT MTS</li></Link>
+        <Link href={'/addmission'}><li className='glass p-1 m-1'>ADMISSION ENQUIRY FORM</li></Link>
+      </ul>
       <div className=" glass-blur md:p-3 p-1 flex justify-center items-center cursor-pointer">
         <Image alt='photo' src="/icons/addmission.png" width={30} height={30} />
       </div>
@@ -47,12 +49,18 @@ const Navbar = () => {
       </Link> */}
       <Link href={'/gallery'}>
         <div className='transition-all icon-parent'>
-        <p className=' glass-blur text-center text-black -mb-6 text-xl p-3'>Gallery</p>
       <div className=" glass-blur md:p-3 p-1 flex justify-center items-center cursor-pointer">
         <Image alt='photo' src="/icons/gallery.png" width={30} height={30} />
       </div>
       </div>
       </Link>
+      <a className=' hover:scale-75 transition-all hover:rotate-12' href="tel:9983760244">
+      <Image alt='photo' src={"/icons/call.png"}  width={40} height={40} />
+     </a>
+     <a className=' hover:scale-75 transition-all hover:rotate-12' href="mailto:contactmtsschool@gmail.com">
+      <Image alt='photo' src={"/icons/mail.png"}  width={40} height={40} />
+     </a>
+    </div>
     </div>
     </>
   )
