23983457bb677d27e9669c187a021e31a083fb15
Lochan Jangid <87118270+lochan-code@users.noreply.github.com>
2023-05-05 17:51:50 -0700

    😮

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 import Navbar from '../components/navbar'
 import '../styles/globals.css'  
+
 import React from 'react'
 function MyApp({ Component, pageProps }) {
   return <>
+  <Head>
+  <meta name="google-site-verification" content="Qs4XobxT757h2HYne5BDi7Cn-FopBwFHyDe-wZEPiBQ" />
+  </Head>
   <Component {...pageProps} />
   <Navbar />
   </>
