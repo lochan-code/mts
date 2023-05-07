fade1da152f927765b978272d30d4a9928ba5734
Lochan Jangid <87118270+lochan-code@users.noreply.github.com>
2023-05-06 20:42:09 -0700

    🚀

--- pages/career.js
+++ pages/career.js
@@ -39,7 +39,7 @@ const Career = () => {
             </h1>
             <div>
               <a
-                href=""
+                href="#career"
                 className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
               >
                 APPLY NOW
@@ -64,7 +64,7 @@ const Career = () => {
             </h1>
           </div>
         </div>
-        <section className="bg-gray-900">
+        <section className="bg-gray-900" id="career">
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
             <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
               <h1 className="mb-4 text-4xl ubuntu tracking-tight font-extrabold text-white">
