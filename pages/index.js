93f51d6e06e8b54ca07e9d6506b8bc8ccb3f75b9
Lochan Jangid <87118270+lochan-code@users.noreply.github.com>
2023-05-30 23:42:44 -0700

    🚀

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
 import Head from 'next/head'
+import { useState } from "react";
 import Image from 'next/image'
 import Link from 'next/link';
 import 'react-toastify/dist/ReactToastify.css';
+import { motion, Variants } from 'framer-motion';
+
+const cardVariants = {
+  offscreen: {
+    x: 300
+  },
+  onscreen: {
+    x: 0,
+    // rotate: -10,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8
+    }
+  }
+};
+
 
 export default function Home() {
   return (
@@ -14,18 +32,24 @@ export default function Home() {
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
         <link rel="apple-touch-icon" href="/logo/favicon.png" />
       </Head>
-      <div>
-        <div className="banner">
-        <section className="bg-white dark:bg-gray-900">
-  <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-    <div className="mr-auto place-self-center lg:col-span-7">
-      <h1 className="max-w-2xl mb-4 text-4xl text-white font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-      MTS School
-      </h1>
-      <p className="max-w-2xl mb-6 font-light  lg:mb-8 md:text-lg lg:text-xl text-gray-200">
-      Mother Teressa Senior Secondary School Baharawanda kalan
-      </p>
-      <a
+      <motion.div 
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true, amount: 0.8 }}>
+      <div className="bg-white dark:bg-gray-900">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl text-white font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+              MTS School
+            </h1>
+            <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-200">
+              Mother Teressa Senior Secondary School Baharawanda kalan
+            </p>
+            <div className="flex flex-wrap">
+            <motion.div  
+            hileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}>
+            <a
         href="mailto:contactmtsschool@gmail.com"
         className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
       >
@@ -43,43 +67,65 @@ export default function Home() {
           />
         </svg>
       </a>
-      <div
+      </motion.div>
+      <motion.div
         className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
         >
       <Link
         href={'/addmission'}
       >
         Admission Form
       </Link>
-      </div>
+      </motion.div>
+      <motion.div  whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}>
       <a href="https://files.appsgeyser.com/MTS%20School%20Mother%20Teressa%20Senior%20Secondary%20School%20Baharawanda%20kalan_17045525.apk?utm_source=email&utm_medium=email&utm_campaign=downloadApp" target="_blank" rel="noreferrer"><button className="bg-gray-300 m-1 hover:bg-gray-400 text-gray-800 font-bold py-2 poppins sm:hidden px-4 rounded inline-flex items-center">
   <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
   <span>Download App</span>
 </button>
   </a>
-    </div>
-    <div className="lg:mt-0 lg:col-span-5 lg:flex m-3">
-      <Image
-        src="/gallery7.jpg" width={500} height={400} priority={true}
-        alt="mts school"
-      />
-    </div>
-  </div>
-</section>
-
-        </div>
-        <div className="flex flex-wrap justify-center m-3 space-x-3">
-          <div className="p-3 flex flex-col glass-blur text-center">
-            <Image alt='student' src="/icons/stundent.png" width={100} height={100} />
-            <p className='threed glass p-1 text-3xl'>500+</p>
+</motion.div>
+</div>
           </div>
-          <div className="p-3 flex flex-col glass-blur text-center">
-            <Image alt='teacher' src="/icons/teacher.png" width={100} height={100} />
-            <p className='threed glass p-1 text-3xl'>15+</p>
-          </div>
+             <motion.div  className="lg:mt-0 lg:col-span-5 lg:flex m-3" variants={cardVariants}>
+            <Image
+              src="/gallery7.jpg"
+              width={500}
+              height={400}
+              priority={true}
+              alt="mts school"
+            />
+          </motion.div>
         </div>
       </div>
-      <div>
+      <div className="flex flex-wrap justify-center m-3 space-x-3">
+      <motion.div
+        className="p-3 flex flex-col bg-blue-500 rounded-md text-center"
+        whileHover={{ scale: 1.1 }}
+        animate={{
+      scale: [1, 2, 2, 1, 1],
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+    }}
+      >
+        <img alt='student' src="/icons/stundent.png" width={100} height={100} />
+        <p className='text-blue-700 text-3xl'>500+</p>
+      </motion.div>
+      <motion.div
+        className="p-3 flex flex-col bg-green-500 rounded-md text-center"
+        whileHover={{ scale: 1.1 }}
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        }}
+      >
+        <img alt='teacher' src="/icons/teacher.png" width={100} height={100} />
+        <p className='text-green-700 text-3xl'>15+</p>
+      </motion.div>
+    </div>
         <h1 className='text-white text-center text-4xl w-full varela'>Facilities</h1>
         <div className="glass-blur flex flex-wrap p-2 justify-center items-center">
           <div className="glass m-3 bg-slate-900 flex justify-center p-3 items-center w=full">
@@ -121,40 +167,38 @@ export default function Home() {
             <p className='ml-1 bungee text-sm text-white'>Students Motivated</p>
           </div>
         </div>
-      </div>
-      <div className="container left-1/2 -translate-x-1/2">
-            <h1 className='text-white text-4xl varela'>Arts</h1>
-            <div className="flex flex-wrap justify-center text-center">
-              <div className="card">
-                <div className="content">
-                  <h2>01</h2>
-                  <h3>HISTORY</h3>
-                  <a href="https://en.wikipedia.org/wiki/History" target="_blank" rel="noreferrer">Read More</a>
-                </div>
-              </div>
-              <div className="card">
-                <div className="content">
-                  <h2>02</h2>
-                  <h3>HINDI LITERATURE</h3>
-                  <a href="https://en.wikipedia.org/wiki/Hindi_literature" target="_blank" rel="noreferrer">Read More</a>
-                </div>
-              </div>
-              <div className="card">
-                <div className="content">
-                  <h2>03</h2>
-                  <h3>GEOGRAPHY</h3>
-                  <a href="https://en.wikipedia.org/wiki/Geography" target="_blank" rel="noreferrer">Read More</a>
-                </div>
-              </div>
-            </div>
-
+      <div className="bg-gray-900 py-12">
+        <h1 className="text-4xl text-white font-bold mb-8 text-center">Arts</h1>
+      <div className="container mx-auto text-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="bg-white rounded-lg shadow-md p-6">
+            <h2 className="text-2xl font-bold mb-4">01</h2>
+            <h3 className="text-lg font-semibold mb-2">HISTORY</h3>
+            <a href="https://en.wikipedia.org/wiki/History" target="_blank" rel="noreferrer"><button class="read-more-button bg-blue-500 text-white text-base px-4 py-2 rounded-md border-none cursor-pointer relative overflow-hidden transform perspective-1000 transition-transform duration-300">
+  Read More
+  <span class="read-more-overlay absolute top-0 left-0 w-full h-full bg-white bg-opacity-10 transform rotate-x-90 translate-z(-100) transition-transform duration-300 opacity-0"></span>
+</button>
+</a>
           </div>
-      <div className="linear-gradient-card-vala p-5">
-        <div className="wrapper flex items-center justify-center p-1">
-          <Image alt='photo' src="/ceo.png"  className='rounded-full' width={100} height={100} />
-          <p className='ml-5 ubuntu text-xl text-gray-500 font-semibold'>Education is important for become a successfull person</p>
+          <div className="bg-white rounded-lg shadow-md p-6">
+            <h2 className="text-2xl font-bold mb-4">02</h2>
+            <h3 className="text-lg font-semibold mb-2">HINDI LITERATURE</h3>
+            <a href="https://en.wikipedia.org/wiki/Hindi_literature" target="_blank" rel="noreferrer" className="text-blue-500 hover:underline"><button class="read-more-button bg-blue-500 text-white text-base px-4 py-2 rounded-md border-none cursor-pointer relative overflow-hidden transform perspective-1000 transition-transform duration-300">
+  Read More
+  <span class="read-more-overlay absolute top-0 left-0 w-full h-full bg-white bg-opacity-10 transform rotate-x-90 translate-z(-100) transition-transform duration-300 opacity-0"></span>
+</button></a>
+          </div>
+          <div className="bg-white rounded-lg shadow-md p-6">
+            <h2 className="text-2xl font-bold mb-4">03</h2>
+            <h3 className="text-lg font-semibold mb-2">GEOGRAPHY</h3>
+            <a href="https://en.wikipedia.org/wiki/Geography" target="_blank" rel="noreferrer" className="text-blue-500 hover:underline"><button class="read-more-button bg-blue-500 text-white text-base px-4 py-2 rounded-md border-none cursor-pointer relative overflow-hidden transform perspective-1000 transition-transform duration-300">
+  Read More
+  <span class="read-more-overlay absolute top-0 left-0 w-full h-full bg-white bg-opacity-10 transform rotate-x-90 translate-z(-100) transition-transform duration-300 opacity-0"></span>
+</button></a>
+          </div>
         </div>
       </div>
+    </div>
       {/* <div className="flex justify-center linear-gradient-card-vala items-center p-10">
         <div className="wrapper w-full flex md:flex-nowrap flex-wrap justify-center md:justify-start items-center md:p-4 p-1">
           <div className="ml-5 md:w-1/2" >
@@ -169,12 +213,52 @@ export default function Home() {
           </div>
         </div>
       </div> */}
-      <div className="flex  justify-center linear-gradient-card-vala items-center p-5">
-        <div className="wrapper bus-service w-full h-full flex items-center ">
-          <Image alt='photo' src="/bus.jpg" className='image-of-bus' width={1000} height={700} />
-          <h1 className='ml-10 ubuntu text-xl font-semibold text-gray-500'>Here bus sevice available. many villages in bus pickup and drop students. prepared driver Bus Driver in our bus service. These are villages where School Bus Service Available - <ul className='list-disc font-light'><li>Piplet</li><li>Kudana</li><li>Kosra</li></ul></h1>
-        </div>
+<div className="flex justify-center p-5">
+  <div className="linear-gradient-card-vala w-full max-w-3xl rounded-lg shadow-lg">
+    <div className="wrapper flex flex-col md:flex-row items-center">
+      <motion.div className="md:w-1/2" variants={cardVariants}>
+        <img alt="bus photo" src="/bus.jpg" className="w-full rounded-lg" />
+      </motion.div>
+      <div className="md:w-1/2 px-8 py-6">
+        <h1 className="text-2xl font-semibold text-gray-500 mb-4">School Bus Service Available</h1>
+        <p className="text-gray-500 mb-4">
+          Our school provides a reliable bus service for students residing in several villages.
+          We have a dedicated team of trained drivers who ensure safe transportation.
+          Here are some of the villages where our bus service is available:
+        </p>
+        <motion.ul
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="list-disc ml-6 text-gray-500"
+        >
+          <motion.li
+            initial={{ opacity: 0, x: -20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+          >
+            Piplet
+          </motion.li>
+          <motion.li
+            initial={{ opacity: 0, x: -20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+          >
+            Kudana
+          </motion.li>
+          <motion.li
+            initial={{ opacity: 0, x: -20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.8 }}
+          >
+            Kosra
+          </motion.li>
+        </motion.ul>
       </div>
+    </div>
+  </div>
+</div>
+
       <div className="flex flex-col justify-center linear-gradient-contact-vala items-center p-5">
         <div className="wrapper">
           <header>Send us a Message</header>
@@ -258,8 +342,9 @@ Send Message<svg
     </ul>
    
   </div>
-</div>
-  <div>.</div>
     </div>
+  <div>.</div>
+</motion.div>
+</div>
   )
 }
